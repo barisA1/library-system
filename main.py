9718d50e30c2ef28fc893fef0dbd840975149df2
barisA1 <aktasb723@gmail.com>
2024-04-06 22:37:52 +0300

    Test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyC')
+    print_hi('study')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
